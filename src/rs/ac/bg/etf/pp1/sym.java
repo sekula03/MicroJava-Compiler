fee7236ac43cb0b8dcd22202219c24af2fd6820c
Sekula <aleksa.se2003@gmail.com>
2026-01-21 19:01:38 +0100

    Leksicka analiza + deo cup specifikacije

---
+++ src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,61 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+    // End of File
+    public static final int EOF = 0;
+
+    // Keywords
+    public static final int PROGRAM = 1;
+    public static final int BREAK = 2;
+    public static final int ENUM = 3;
+    public static final int CLASS = 4;
+    public static final int ABSTRACT = 5;
+    public static final int ELSE = 6;
+    public static final int CONST = 7;
+    public static final int IF = 8;
+    public static final int NEW = 9;
+    public static final int PRINT = 10;
+    public static final int READ = 11;
+    public static final int RETURN = 12;
+    public static final int VOID = 13;
+    public static final int EXTENDS = 14;
+    public static final int CONTINUE = 15;
+    public static final int FOR = 16;
+    public static final int LENGTH = 17;
+    public static final int SWITCH = 18;
+    public static final int CASE = 19;
+
+    // Operators and Punctuators
+    public static final int PLUS = 20;
+    public static final int MINUS = 21;
+    public static final int MUL = 22;
+    public static final int DIV = 23;
+    public static final int MOD = 24;
+    public static final int EQUAL = 25;
+    public static final int NOT_EQUAL = 26;
+    public static final int GREATER = 27;
+    public static final int GREATER_EQUAL = 28;
+    public static final int LESS = 29;
+    public static final int LESS_EQUAL = 30;
+    public static final int AND = 31;
+    public static final int OR = 32;
+    public static final int ASSIGN = 33;
+    public static final int INC = 34;
+    public static final int DEC = 35;
+    public static final int SEMI = 36;
+    public static final int COLON = 37;
+    public static final int COMMA = 38;
+    public static final int DOT = 39;
+    public static final int LPAREN = 40;
+    public static final int RPAREN = 41;
+    public static final int LBRACKET = 42;
+    public static final int RBRACKET = 43;
+    public static final int LBRACE = 44;
+    public static final int RBRACE = 45;
+
+    // Identifiers and Constants
+    public static final int IDENT = 46;
+    public static final int NUM_CONST = 47;
+    public static final int CHAR_CONST = 48;
+    public static final int BOOL_CONST = 49;
+}
